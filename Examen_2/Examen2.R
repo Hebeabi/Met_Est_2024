5bdc0f77eb96b9ccbbcc8116a39d82cdda6a1b87
Hebeabi <hebe.trejo18@gmail.com>
2024-12-04 12:23:57 -0600

    Delete Examen_2 directory

--- Examen_2/Examen2.R
+++
@@ -1,6 +0,0 @@
-set.seed(42)
-n <- 30
-altura <- rnorm(n, mean = 170, sd= 10) #altura en cm
-peso <- 0.5 * altura + rnorm(n,mean = 0, sd =5) #peso en kg, con algo de ruido
-t.test(altura, peso)
-cor.test(altura,peso)
